30752a5adc11502479fe6ca8f10669bd2f1a9c0d
Cole Helsell <chelsell@iq218.(none)>
2015-09-29 18:40:04 -0700

    Fixed the formatting of the loops files

--- loops/make_loop_files.py
+++ loops/make_loop_files.py
@@ -5,7 +5,7 @@ ends = [1363, 1369]
 
 for start in starts:
     for end in ends:
-        loop_spec = '{0} {1} {1} 0 1'.format(start, end)
+        loop_spec = 'LOOP {0} {1} {1} 0 1'.format(start, end)
         loop_name = '{0}_{1}.loop'.format(start, end)
 
         with open(loop_name, 'w') as file:
